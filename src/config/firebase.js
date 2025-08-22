0197b4bea68a5dd84f1d1eeee34149bc8c55e3b7
Ashraf Mohamed <ashraflkj21@gmail.com>
2025-08-23 02:07:51 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRjk-j1bOEJsNZGEwgf5A3vUvvQe74Src",
+  apiKey: 'enter your key',
   authDomain: "edusity-4d4a7.firebaseapp.com",
   projectId: "edusity-4d4a7",
   storageBucket: "edusity-4d4a7.firebasestorage.app",
